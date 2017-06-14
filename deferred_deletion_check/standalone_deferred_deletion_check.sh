73acb1413fe5958067839d4cb0028164f215512b
Matt Bentley <mbentley@mbentley.net>
2017-06-14 08:56:15 -0400

    Added kernel output
    Signed-off-by: Matt Bentley <mbentley@mbentley.net>

--- deferred_deletion_check/standalone_deferred_deletion_check.sh
+++ deferred_deletion_check/standalone_deferred_deletion_check.sh
@@ -9,6 +9,8 @@ fi
 # shellcheck disable=SC1091 source=/dev/null
 . "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/check_for_deferred_deletion.sh
 
+echo "Kernel version: $(uname -r)"
+
 if platform_supports_deferred_deletion
 then
   echo "Deferred deletion is supported"
